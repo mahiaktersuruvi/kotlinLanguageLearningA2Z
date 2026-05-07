93beff1d5a250f420ee6afa406cdcbc293fe21d4
mahiaktersuruvi <mahiaktersuruvi2@gmail.com>
2026-05-07 12:22:13 +0600

    first commit

---
+++ app/src/androidTest/java/com/example/kotlinlanguage/kotlin.kt
@@ -0,0 +1,28 @@
+// print('hello world')
+
+/*fun main(){
+    println("hello")
+}
+*/
+// Variable
+/* fun main(){
+    var name:String=" Suruvi"
+    name="Ety"
+    val age:Int=20
+    println(name)
+    println(age)
+}*/
+
+// conditional
+/*fun main(){
+    val number=79
+
+    if (number > 80){
+        println("Welcome, you have done well, $number")
+    }else{
+        println("You have to do more hardwork")
+    }
+}
+*/
+
+
